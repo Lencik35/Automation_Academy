c397426ca6823bcdd8b6017dd86de99a8832914a
ESTOLEAR <stolearelena88@gmail.com>
2024-05-05 13:45:20 +0300

    homework

---
+++ src/Homework/ex1_Subtraction.java
@@ -0,0 +1,11 @@
+package Homework;
+
+public class ex1_Subtraction {
+    public static void main(String[] args) {
+
+    int a = 1;
+    int b = 5;
+    int substraction = b / a;
+        System.out.println("Substraction: "+substraction);
+}
+}
---
+++ src/Homework/ex2_Temperature_converter.java
@@ -0,0 +1,9 @@
+package Homework;
+
+public class ex2_Temperature_converter {
+    public static void main(String[] args) {
+        double fahrenheit=98.6;
+        double celsius=(fahrenheit-32)*5/9;
+        System.out.println("Fahrenheit "+ fahrenheit+" = " +celsius + " Celsius");
+    }
+}
